247198c1e3af4a9f61ebcf62670a021c0547dc01
2084246s <2084246S@student.gla.ac.uk>
2015-11-26 16:53:30 +0000

    basic popup register and login boxes

--- getgrub/home.js
+++ getgrub/home.js
@@ -1,9 +1,3 @@
 $(document).ready(function() {
-    function login(showhide){
-if(showhide == "show"){
-    document.getElementById('popupbox').style.visibility="visible";
-}else if(showhide == "hide"){
-    document.getElementById('popupbox').style.visibility="hidden"; 
-}
-}
+   
 });
